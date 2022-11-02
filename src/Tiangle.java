2319470d59762b5a024dc90525a01d3a591b3f88
Baiel <kanaibekbaiell@mai.com>
2022-11-02 05:45:03 +0300

    commit

---
+++ src/Tiangle.java
@@ -0,0 +1,15 @@
+
+public class Tiangle {
+    int a;
+    int b;
+    int c;
+
+    public static void joob(){
+        Tiangle tiangle=new Tiangle();
+        tiangle.a=4;
+        tiangle.b=12;
+        tiangle.c=8;
+        System.out.println(tiangle.a*tiangle.c/2);
+
+
+    }}
